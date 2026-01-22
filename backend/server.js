35054e0ba4e809e61b780d6e050258e9aa46ece2
LivinkumarStackly <livinkumar@thestackly.com>
2026-01-22 18:00:37 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(morgan('dev'));
 // CORS configuration - allow frontend access
 app.use(cors({
     origin: ['http://localhost:5173',
-        'https://expense-tracker-pachamuthu.vercel.app/'],
+        'https://expense-tracker-pachamuthu.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
